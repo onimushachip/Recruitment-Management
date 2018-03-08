88e21452f1fa0480eb49995066952f61d9983320
tbah <tiemby05@gmail.com>
2018-03-08 15:03:28 -0500

    add user and job APIs

--- Server/server.js
+++ Server/server.js
@@ -39,6 +39,35 @@ var applicant = mongoose.model("Applicant", mongoose.Schema({
     recruiterUserId: String
 }));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/client'));
+
+//Add a user to the userinfo collection Thierno
+app.post('/api/addUserInfo', function(req, res){
+    userInfo.create(req.body, function(err, usr){
+        if(err)
+            res.send(err); 
+        console.log(usr);
+        res.json(usr);
+    });
+});
+
+//Add a user to the userinfo collection Thierno
+app.post('/api/addJobPosting', function(req, res){
+    jobPostingInfo.create(req.body, function(err, job){
+        if(err)
+            res.send(err); 
+        console.log(job);
+        res.json(job);
+    });
+});
+
 //Start the API server
 app.listen(3000, function() {
     console.log("Recruitment Server is up!!!");
